dee6f05948d1a617af4bedb570694e6c26676d25
Vitaliy Ryepnoy <vitdotonline@gmail.com>
2016-07-29 21:42:16 +1000

    step #05 update migration, change schema 'users'

--- tdd-tweeter/database/migrations/2014_10_12_000000_create_users_table.php
+++ tdd-tweeter/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('username');
             $table->rememberToken();
             $table->timestamps();
         });
