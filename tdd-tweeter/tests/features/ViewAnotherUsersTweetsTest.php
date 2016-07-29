31be7da9fbddd38aadad129b2371a2bea6b1ea69
Vitaliy Ryepnoy <vitdotonline@gmail.com>
2016-07-29 18:21:11 +1000

    step #01 create testViewingAnotherUsersTweets

---
+++ tdd-tweeter/tests/features/ViewAnotherUsersTweetsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewAnotherUsersTweetsTest extends TestCase
+{
+    public function testViewingAnotherUsersTweets()
+    {
+        $user = factory(User::class)->create([
+            'username' => 'johndoe'
+        ]);
+        $tweet = factory(Tweet::class)->make([
+            'body' => 'My first tweet'
+        ]);
+
+        $user->tweets()->save($tweet);
+
+        $this->visit('/johndoe')
+            ->see('My first tweet');
+    }
+}
